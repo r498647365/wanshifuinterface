c24335dcc434e554184ae8e64d2b6c9bfbac0615
Administrator <Administrator@PCOS-1810151807>
2019-03-18 17:28:14 +0800

    第一次提交

---
+++ src/main/java/com/wanshifu/myinterface2/myinterface2/App.java
@@ -0,0 +1,13 @@
+package com.wanshifu.myinterface2.myinterface2;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
